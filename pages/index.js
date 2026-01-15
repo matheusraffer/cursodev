f1df598ae3e1ee8e13e55a7ada86e0c8d406e7a9
matheusraffer <72952801+matheusraffer@users.noreply.github.com>
2026-01-15 21:42:01 +0000

    Alterado o conteúdo da página

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
 function Home() {
-  return <h1>Mattab</h1>;
+  return (
+    <div style={styles.container}>
+      <Image src="/assets/ela.jpg" alt="Imagem" width={1000} />
+
+      <h1 style={styles.title}>Te amo ❤️</h1>
+    </div>
+  );
 }
 
 export default Home;
