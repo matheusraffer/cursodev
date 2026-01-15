8fa880f2dd6dfdaa9115fa44872dadba60217849
matheusraffer <72952801+matheusraffer@users.noreply.github.com>
2026-01-15 22:39:02 +0000

    Alterado o conteúdo da página

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,10 @@ import styles from "../styles/Home.module.css";
 
 function Home() {
   return (
-    <div style={styles.container}>
-      <Image src="/assets/ela.jpg" alt="Imagem" width={1000} />
+    <div className={styles.container}>
+      <Image src="/assets/ela.jpg" alt="Imagem" width={1000} height={600} />
 
-      <h1 style={styles.title}>Te amo ❤️</h1>
+      <h1 className={styles.title}>Te amo ❤️</h1>
     </div>
   );
 }
